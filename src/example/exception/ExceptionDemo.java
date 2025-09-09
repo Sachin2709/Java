9526cc306bc974f9060c3edf2d7c0b9b471ee23b
sachi <sachi@10.21.131.147>
2025-09-09 11:42:17 +0530

    09/09/2025 Exception Handling

---
+++ src/example/exception/ExceptionDemo.java
@@ -0,0 +1,20 @@
+package example.exception;
+
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		System.out.println("1");
+		try {
+	int a = 5, b = 0;
+	System.out.println("2");
+	int c = a/b;
+	System.out.println("3");
+	System.out.println("4");
+		}catch(ArithmeticException e) {
+			System.out.println("The exception is handled");
+		}
+		System.out.println("5");
+		System.out.println("6");
+	
+	}
+	
+}
